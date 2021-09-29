74a0631ba914ed4639c01bb24c9f30f4bed4b30b
yash7047 <58131599+yash7047@users.noreply.github.com>
2021-09-29 15:21:46 +0530

    remove database error

--- mongoose.js
+++ mongoose.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
-const db_port = process.env.url || 'mongodb://localhost:27017/movie-app'
+//const db_port = process.env.url || 'mongodb://localhost:27017/movie-app'
+
+const db_port = 'mongodb+srv://yash_7047:Yash@1234@cluster0.mqwds.mongodb.net/movie?retryWrites=true&w=majority'
 
 mongoose.connect(db_port,{
     useNewUrlParser: true,
